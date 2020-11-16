137f426dd3d6c470cfdfe8c0633fadff88b42d49
jingnie0222 <45628149+jingnie0222@users.noreply.github.com>
2020-11-16 18:01:55 +0800

    Update test_toutiao.py
    add  yiweici group

--- test_toutiao.py
+++ test_toutiao.py
@@ -155,6 +155,25 @@ for k,v in ret.items():
     output = "Day {0}: Pill # {1}". format(k, v)
     print(output)
 
+ 
+# 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+# 示例：
+# 输入：["eat","tea","tan","ate","nat","bat"],
+# 输出：[["ate","eat","tea"], ["nat","tan"], ["bat"]]
+# 说明：
+# 所有输入均为小写字母。
+# 不考虑答案输出的顺序。
+
+def group(strs):
+    str_dict = {}
+    for str in strs:
+        tmp = "".join(sorted(str))
+        if tmp in str_dict:
+            str_dict[tmp].append(str)
+        else:
+            str_dict[tmp] = [str]
+
+    return [val for val in str_dict.values()]
 
 
 if __name__ == "__main__":
